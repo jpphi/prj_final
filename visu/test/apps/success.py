cf56753d194afeab3f962ff71c2e820fc902d328
jpphi <jpphi@yahoo.fr>
2022-03-31 08:08:03 +0200

    avancement prj

--- visu/test/apps/success.py
+++ visu/test/apps/success.py
@@ -3,18 +3,76 @@ from dash import dcc
 from dash import html
 #import dash
 
+from dash.dependencies import Input, Output, State
 
 import dash_bootstrap_components as dbc
 
+from app import app
+
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("data", href="/data"),
+        dbc.DropdownMenuItem("success", href="/success"),
+        dbc.DropdownMenuItem("logout", href="/logout"),
+        dbc.DropdownMenuItem("login", href="/login")
+    ],
+    nav = True,
+    in_navbar = True,
+    label = "Explore",
+)
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src="./images/image_test.png", height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Emotions Wheel DASH", className="ml-2")),
+                    ],
+                    align="center",
+                    #no_gutters=True,
+                ),
+                href="/",
+            ),
+            dbc.NavbarToggler(id="navbar-toggler2"),
+            dbc.Collapse(
+                dbc.Nav(
+                    [dropdown], className="ml-auto", navbar=True
+                ),
+                id="navbar-collapse2",
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    className="mb-4",
+)
+
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+for i in [2]:
+    app.callback(
+        Output(f"navbar-collapse{i}", "is_open"),
+        [Input(f"navbar-toggler{i}", "n_clicks")],
+        [State(f"navbar-collapse{i}", "is_open")],
+    )(toggle_navbar_collapse)
 
 
-layout = html.Div([dcc.Location(id='url_login_success', refresh=True)
-            , html.Div([html.H2('Login successful.')
-                    , html.Br()
-                    , html.P('Select a Dataset')
-                    , dcc.Link('Data', href = '/data')
-                ]) #end div
-            , html.Div([html.Br()
-                    , html.Button(id='back-button', children='Go back', n_clicks=0)
+
+
+layout = html.Div([dcc.Location(id='url_login_success', refresh=True),
+            navbar,
+            html.Div([html.H2('Login successful.'),
+            html.Br(),
+            html.P('Select a Dataset'),
+            dcc.Link('Data', href = '/data'),
+            ]), #end div
+            html.Div([html.Br(),
+            html.Button(id='back-button', children='Go back', n_clicks=0),
                 ]) #end div
         ]) #end div
--- visu/test/testlog.py
+++ visu/test/testlog.py
@@ -5,7 +5,6 @@ from dash import dcc
 #import dash_html_components as html
 from dash import html
 import dash
-from dash.dependencies import Input, Output, State
 from sqlalchemy import Table, create_engine
 from sqlalchemy.sql import select
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +14,8 @@ import warnings
 import os
 from flask_login import login_user, logout_user, current_user, LoginManager, UserMixin
 import configparser
+
+from dash.dependencies import Input, Output, State
 
 import dash_bootstrap_components as dbc
 
@@ -61,83 +62,23 @@ login_manager.login_view = '/login'
 class Users(UserMixin, Users):
     pass
 
-dropdown = dbc.DropdownMenu(
-    children=[
-        dbc.DropdownMenuItem("data", href="/data"),
-        dbc.DropdownMenuItem("success", href="/success"),
-        dbc.DropdownMenuItem("logout", href="/logout"),
-        dbc.DropdownMenuItem("login", href="/login")
-    ],
-    nav = True,
-    in_navbar = True,
-    label = "Explore",
-)
-
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            html.A(
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src="./images/image_test.png", height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Emotions Wheel DASH", className="ml-2")),
-                    ],
-                    align="center",
-                    #no_gutters=True,
-                ),
-                href="/",
-            ),
-            dbc.NavbarToggler(id="navbar-toggler2"),
-            dbc.Collapse(
-                dbc.Nav(
-                    [dropdown], className="ml-auto", navbar=True
-                ),
-                id="navbar-collapse2",
-                navbar=True,
-            ),
-        ]
-    ),
-    color="dark",
-    dark=True,
-    className="mb-4",
-)
-
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
-
-for i in [2]:
-    app.callback(
-        Output(f"navbar-collapse{i}", "is_open"),
-        [Input(f"navbar-toggler{i}", "n_clicks")],
-        [State(f"navbar-collapse{i}", "is_open")],
-    )(toggle_navbar_collapse)
 
 
-
-
-
-
+"""
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     navbar,
     html.Div(id='page-content', className='content'),
 ])
-
-
-
-
-
-
 """
+
+
 
 app.layout= html.Div([
             html.Div(id='page-content', className='content')
             ,  dcc.Location(id='url', refresh=False)
         ])
 
-"""
 
 
 
