64d0cd701dab2b1753465987d0e70444308f7463
jpphi <jpphi@yahoo.fr>
2022-04-19 07:13:19 +0200

    création de visu dans E1

---
+++ E1/visu/apps/glob.py
@@ -0,0 +1,13 @@
+Bleu_de_minuit= "#003366"
+
+Bleu_marine= "#03224C"
+
+Bleu_nuit= "#OF055B"
+
+Bleu_outremer= "#15019B"
+
+Bleu_outremer2= "#2B009A"
+
+fond_ecran_reponse= "#007B94"
+
+fond_ecran_formulaire= Bleu_de_minuit
